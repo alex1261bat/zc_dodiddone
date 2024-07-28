483d5e8f2d13bdab8b533838ae69b76c1f5864da
alex1261bat <alex126.1bat@gmail.com>
2024-07-28 23:55:05 +0300

    autologin

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,17 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:zc_dodiddone/pages/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:zc_dodiddone/services/firebase_auth.dart';
 import 'package:zc_dodiddone/theme/theme.dart';
-
 import '../pages/login_page.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService _authService = AuthService(); // Initialize your AuthService
+  late User? user;
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    user = _authService.currentUser;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: DoDidDoneTheme.lightTheme,
-      home: const LoginPage(),
+      home: user == null ? const LoginPage() : const MainPage(),
     );
   }
 }
